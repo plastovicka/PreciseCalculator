4a3722b0ee8a75750b4194883ab9adc7447c04b2
Petr Laštovička <plastovicka@users.noreply.github.com>
2014-02-01 21:17:37 +0000

    fixed CMPU did not trim arguments, norm also calls trim

--- source/aritm-masm.asm
+++ source/aritm-masm.asm
@@ -1,4 +1,4 @@
-; (C) 2005-2011  Petr Lastovicka
+; (C) 2005-2014  Petr Lastovicka
  
 ; This program is free software; you can redistribute it and/or
 ; modify it under the terms of the GNU General Public License.
@@ -432,12 +432,15 @@ round2:	mov	eax,[esi]
 inclen:	inc	dword ptr [edi-12]
 	ret
 ;-------------------------------------
-;normalizace [edi] - mantisa nebude zaèínat na nulu
+;normalizace [edi] - mantisa nebude zaèínat ani konèit na nulu
 ;zmìní esi,edi
 norm	proc
+	cmp	dword ptr [edi-12],0
+	jbe	@@ret    ;zlomek nebo nula
+	call	trim
 	mov	ecx,[edi-12]
 	test	ecx,ecx
-	jz	@@ret
+	jz	@@ret   ;trim výsledek vynulovalo
 	xor	edx,edx
 	cmp	[edi],edx
 	jnz	@@ret
@@ -708,16 +711,25 @@ cmpu	proc	uses esi edi
 	cmp	eax,[ecx-4]
 	jg	@@gr1
 	jl	@@gr2
-;nejvyšší øád
+;rychlé porovnání nejvyššího øádu
 	mov	eax,[edx]
 	cmp	eax,[ecx]
 	jnz	@@ret
+;oøíznutí koncových nul
 	mov	eax,[edx-12]
-	lea	esi,[edx+4*eax]
-	mov	eax,[ecx-12]
-	lea	edi,[ecx+4*eax]
 	add	edx,4
+	lea	esi,[edx+4*eax]
+@@t1:	sub	esi,4
+	mov	eax,[esi-4]
+	test	eax,eax
+	jz	@@t1
+	mov	eax,[ecx-12]
 	add	ecx,4
+	lea	edi,[ecx+4*eax]
+@@t2:	sub	edi,4
+	mov	eax,[edi-4]
+	test	eax,eax
+	jz	@@t2
 ;mantisy
 @@lp:	cmp	edx,esi
 	jnc	@@2
@@ -2719,8 +2731,7 @@ READX1	proc	uses esi edi ebx a1,buf
 	jmp	@@d0
 ;uøíznutí koncových nul a normalizace
 @@de:	pop	edi
-@@trim:	call	trim
-	call	norm
+@@trim:	call	norm
 	pop	eax
 	ret
 @@end:	push	esi
